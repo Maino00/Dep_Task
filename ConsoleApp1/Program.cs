b9210c896a2fd222d20430ce63ee658f023ad4f3
USsefRashed <USsefRashed@gmail.com>
2022-12-21 15:16:01 +0200

    fixing program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ public class Department
     public double Salary
     {
         get;
-        private set;
+        set;
     }
 
     public Department(string name , float compensation)
@@ -49,73 +49,104 @@ public class Employee
     
 public class Database
 {
-    private int employee;
-    private int department;
+    private int eLength;
+    private int dLength;
     public Department[] Dep = new Department[20];
     public Employee[] Emp = new Employee[20];
+    public Database()
+    {
+        eLength = 0;
+        dLength = 0;
+    }
     public void AddDepartment(Department dep)
     {
-        Dep[department ++]= dep;
+        Dep[dLength]= dep;
+        dLength++;
     }
     public void AddEmployee(Employee emp)
     {
-        Emp[employee ++]= emp;
-    } 
+        Emp[eLength]= emp;
+        eLength++;
+    }
+    public Department getDepartmentByIndex(int index)
+    {
+        if (index < 0 || index >dLength) {
+            Console.WriteLine("Out of Range index");
+            return null;
+        }
+        return Dep[index];
+    }
+    public void DisplayAllDepartments()
+    {
+
+        for (int i = 0; i < dLength; i++)
+        {
+            Console.WriteLine($"{i} == for ==> {Dep[i].Name}");
+        }
+    }
+
 }
 
 public class OOP_Task
 {
     private static void Main()
     {
+        bool key = true;
         var database = new Database();
-        while (true)
+        while (key)
         {
-            Console.WriteLine("1) Add a department  2) Add an Employee");
+            Console.WriteLine("1) Add a department  2) Add an Employee 3)Exit");
+
+            var option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.Write("Name: ");
+                    var name = Console.ReadLine();
+
+                    Console.Write("Compensation: ");
+                    var compensation = float.Parse(Console.ReadLine());
+
+                    var dept = new Department(name, compensation);
+                    database.AddDepartment(dept);
+
+                    break;
+                case 2:
+                    Console.Write("First name: ");
+                    var firstName = Console.ReadLine();
+
+                    Console.Write("Last name: ");
+                    var lastName = Console.ReadLine();
+
+                    Console.Write("Salary: ");
+                    var Salary = Convert.ToDouble(Console.ReadLine());
+                    database.DisplayAllDepartments();
+
+
+                    Console.Write("Department: ");
+                    int index = int.Parse( Console.ReadLine());
+                    Department d = database.getDepartmentByIndex(index);
+
+                    d.Salary = Salary;
+
+                    var emp = new Employee(firstName, lastName, Salary,d);
+                    Console.WriteLine(emp.Dept.Salary);
+                    database.AddEmployee(emp);
+
+                    break;
+                case 3:
+                    key = false;
+                    break;
+                default:
+                    Console.WriteLine("Try another option");
+                    option = Convert.ToInt32(Console.ReadLine());
+                    return;
+
+
+
+            }
         }
-        var option = Convert.ToInt32(Console.ReadLine());
-
-        switch (option)
-        {
-            case 1:
-                Console.Write("Name: ");
-                var name = Console.ReadLine();
-
-                Console.Write("Compensation: ");
-                var compensation = Convert.ToSingle(Console.ReadLine());
-
-                Console.Write("Salary: ");
-                var salary = Convert.ToSingle(Console.ReadLine());
-
-                var dept = new Department(name, compensation);
-                database.AddDepartment(dept);
-
-                break;
-            case 2:
-                Console.Write("First name: ");
-                var firstName = Console.ReadLine();
-
-                Console.Write("Last name: ");
-                var lastName = Console.ReadLine(); 
-
-                Console.Write("Salary: ");
-                var Salary = Convert.ToSingle(Console.ReadLine());
-
-                Console.Write("Department: ");
-                var department = Console.ReadLine();
-
-                var emp = new Employee(firstName, lastName, Salary, dept);
-                database.AddEmployee(emp);
-
-                break;
-            default:
-                Console.WriteLine("Try another option");
-                 option = Convert.ToInt32(Console.ReadLine());
-                return;
-
-               
-                
-        }
-             
         
     }
 
